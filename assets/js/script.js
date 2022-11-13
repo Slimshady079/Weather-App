10593d6082ea1a8e56f23a5fef400aceb4e5975b
max <maximiliangibes@gmail.com>
2022-11-13 12:11:55 -0700

    added dynamic elements to append to empty section and show user input on html not console

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@
 
 var userInput = document.getElementById("user-input");
 var userForm = document.getElementById("form-submit");
+var dayForecast = document.querySelector("#current-forecast");
+var weekForecast;
 // var dayForcast = document.getElementById("current-forecast");
 
 //https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
@@ -11,39 +13,85 @@ var userForm = document.getElementById("form-submit");
 //30.2711286 - lat
 //-97.7436995 - lon
 
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
 //put the weather api here
-var weatherAPI = "https://api.openweathermap.org/data/3.0/onecall?";
+var weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?";
 
-var apiKey = "539c07666f83fa4cdcc3f88fe2d19132";
+//var apiKey = "539c07666f83fa4cdcc3f88fe2d19132";
 
-function fetchWeather(city) {
-  console.log(city);
+var apiKey = "d91f911bcf2c0f925fb6535547a5ddc9";
+//responsible for finding lat and lon from input value
+function fetchCoordinates(city) {
+  //console.log(city);
+  var rootEndPoint = "http://api.openweathermap.org/geo/1.0/direct?";
+  var apiCall = rootEndPoint + "q=" + city + "&appid=" + apiKey;
+  console.log(apiCall);
+
+  fetch(apiCall)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      //console.log(lat);
+      //console.log(lon);
+      fetchWeather(lat, lon);
+    });
 }
 
-var apiCall =
-  weatherAPI +
-  "lat=30.2711286" +
-  "&lon=-97.7436995" +
-  "&appid=539c07666f83fa4cdcc3f88fe2d19132";
-console.log(apiCall);
+function fetchWeather(lat, lon) {
+  console.log(lat);
+  console.log(lon);
+  var apiCall =
+    weatherAPI +
+    "lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&units=imperial" +
+    "&appid=d91f911bcf2c0f925fb6535547a5ddc9";
+  //console.log(apiCall);
 
-fetch(apiCall)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+  fetch(apiCall)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      //var h1El = $("<h1>");
+      var h1El = document.createElement("h1");
+      var pTemp = document.createElement("p");
+      //var p2 = $("<p>");
+      var pWind = document.createElement("p");
+      var pHumid = document.createElement("p");
+      var date = document.createElement("h1");
+
+      $(date).text(data.list[0].dt_txt);
+      $(pTemp).text("Temperature: " + data.list[0].main.temp + " Farrenheit");
+      $(pWind).text("Wind speed: " + data.list[0].wind.speed + " MPH");
+      $(pHumid).text("Humidity: " + data.list[0].main.humidity + "%");
+
+      dayForecast.append(date);
+      dayForecast.append(pTemp);
+      dayForecast.append(pWind);
+      dayForecast.append(pHumid);
+    });
+}
 
 function handleFormSubmit(e) {
   e.preventDefault();
-  console.log("still beastin'");
+  //console.log("inside handleFormSubmit function");
   var input = userInput.value;
-
-  fetchWeather(input);
+  // fetchWeather(input);
+  fetchCoordinates(input);
 }
 // //event listeners
 userForm.addEventListener("submit", handleFormSubmit);
+var cities = [];
+//localStorage.setItem("input", JSON.stringify(cities));
 
 //local storage
 
